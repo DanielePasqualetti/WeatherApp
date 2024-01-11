2e05e22d470da4885120b8bd03035ac89cb11185
DanielePasqualetti <129083893+DanielePasqualetti@users.noreply.github.com>
2024-01-11 17:29:30 +0100

    deploy

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAvZWflnzsT0HCr_efWmYbMbdUcZ10ifQA',
+  authDomain: 'weatherapp-dc236.firebaseapp.com',
+  projectId: 'weatherapp-dc236',
+  storageBucket: 'weatherapp-dc236.appspot.com',
+  messagingSenderId: '82091149868',
+  appId: '1:82091149868:web:8e800b7f42311e0ef59f89',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
