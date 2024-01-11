88ac9ff023aed9b0b3f35d0a32edd8ff1ec92274
DanielePasqualetti <129083893+DanielePasqualetti@users.noreply.github.com>
2024-01-11 17:48:06 +0100

    deploy2

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,22 @@ platformBrowserDynamic()
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAvZWflnzsT0HCr_efWmYbMbdUcZ10ifQA',
-  authDomain: 'weatherapp-dc236.firebaseapp.com',
-  projectId: 'weatherapp-dc236',
-  storageBucket: 'weatherapp-dc236.appspot.com',
-  messagingSenderId: '82091149868',
-  appId: '1:82091149868:web:8e800b7f42311e0ef59f89',
+  apiKey: 'AIzaSyA8pqSIZcPXURlPs1Nq334rb-DcxwTXR3k',
+  authDomain: 'weatherapp-25a91.firebaseapp.com',
+  projectId: 'weatherapp-25a91',
+  storageBucket: 'weatherapp-25a91.appspot.com',
+  messagingSenderId: '503902143057',
+  appId: '1:503902143057:web:4dfcb261bd1c8aa9c7ee70',
+  measurementId: 'G-SKT70NZS1X',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
